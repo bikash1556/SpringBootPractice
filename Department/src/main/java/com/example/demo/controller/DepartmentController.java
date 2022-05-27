6fd868154eb4f66e6d6794ddce76d016c0444ac1
Bikash <bikashsutar01@gmail.com>
2022-05-27 15:45:57 +0530

    Update DepartmentController.java
    added slf4j instance of lombok for logging

--- Department/src/main/java/com/example/demo/controller/DepartmentController.java
+++ Department/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -15,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class DepartmentController {
-	
-	Logger log=LoggerFactory.getLogger(DepartmentController.class);
-	
+
 	@Autowired
 	private DepartmentService departmentService;
 
